f7dce134ca3a1997eac88edb7053344079ad32f8
Conor Pickering <c.pickering1@universityofgalway.ie>
2025-04-18 14:22:26 +0100

    remapping movement axes

--- notspot_sim_py/src/notspot_navigation/scripts/cmd_vel_to_joints.py
+++ notspot_sim_py/src/notspot_navigation/scripts/cmd_vel_to_joints.py
@@ -200,8 +200,8 @@ class CmdVelToJoints:
         # command.yaw_rate = msg.axes[2] * self.max_yaw_rate      # Rotation - AXIS 2
         
         # STANDARD MAPPING (Should match TrotGaitController.updateStateCommand)
-        joy_msg.axes[2] = -linear_x    # Forward/backward on axis 3 
-        joy_msg.axes[1] = linear_y    # Left/right on axis 0  
+        joy_msg.axes[2] = linear_x    # Forward/backward on axis 3 
+        joy_msg.axes[0] = linear_y    # Left/right on axis 0  
         joy_msg.axes[3] = angular_z   # Rotation on axis 2
         
         # Update last command time
